85bd1075cfe65173c4eac025e7994d04ece595e1
Saoud Alfares <sfares85@gmail.com>
2019-09-22 10:31:36 +0300

    added model "address" and field to "profile"

--- api/models.py
+++ api/models.py
@@ -4,24 +4,24 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Brand(models.Model):
-    name = models.CharField(max_length = 100)
+	name = models.CharField(max_length = 100)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 class Watch(models.Model):
 	brand = models.ForeignKey(Brand, on_delete=models.CASCADE,  related_name="brand")
 	model_name = models.CharField(max_length=100)
 	case_size = models.IntegerField(validators=[MinValueValidator(20),
-                                       MaxValueValidator(60)])
+									   MaxValueValidator(60)])
 	thickness = models.IntegerField(validators=[MinValueValidator(5),
-                                       MaxValueValidator(30)],blank=True,null=True)
+									   MaxValueValidator(30)],blank=True,null=True)
 	lug_width = models.IntegerField(validators=[MinValueValidator(10),
-                                       MaxValueValidator(30)],blank=True,null=True)
+									   MaxValueValidator(30)],blank=True,null=True)
 	lug_to_lug = models.IntegerField(validators=[MinValueValidator(30),
-                                       MaxValueValidator(70)],blank=True,null=True)
+									   MaxValueValidator(70)],blank=True,null=True)
 	water_resistance = models.IntegerField(validators=[MinValueValidator(0),
-                                       MaxValueValidator(20000)],blank=True,null=True)
+									   MaxValueValidator(20000)],blank=True,null=True)
 	manufacture_year = models.IntegerField(validators=[MinValueValidator(1700)],blank=True,null=True)
 	price = models.DecimalField(max_digits=10, decimal_places=3)
 	user = models.ForeignKey(User, on_delete=models.CASCADE,  related_name="watches")
@@ -80,19 +80,19 @@ class Profile(models.Model):
 		return str(self.user)
 
 class Address (models.Model):
-    COUNTRIES = (
-        ('Kuwait', 'Kuwait'),
-        ('Oman','Oman'),
-        ('UAE','UAE'),
-        ('KSA','KSA'),
-        ('Bahrain','Bahrain'),
-        ('Qatar','Qatar'),
-        ('other','other'),
-    )
-    user = models.OneToOneField(User , on_delete = models.CASCADE)
-    country = models.CharField( choices = COUNTRIES , max_length = 20, default='Kuwait')
-    city = models.CharField( max_length = 100, blank = True)
-    governate = models.CharField( max_length = 100, blank = True)
-    zipcode = models.CharField(max_length = 5, blank = True)
-    street_line1 = models.CharField( max_length = 100, blank = True)
-    street_line2 = models.CharField( max_length = 100, blank = True)
+	COUNTRIES = (
+		('Kuwait', 'Kuwait'),
+		('Oman','Oman'),
+		('UAE','UAE'),
+		('KSA','KSA'),
+		('Bahrain','Bahrain'),
+		('Qatar','Qatar'),
+		('other','other'),
+	)
+	user = models.OneToOneField(User , on_delete = models.CASCADE)
+	country = models.CharField( choices = COUNTRIES , max_length = 20, default='Kuwait')
+	city = models.CharField( max_length = 100, blank = True)
+	governate = models.CharField( max_length = 100, blank = True)
+	zipcode = models.CharField(max_length = 5, blank = True)
+	street_line1 = models.CharField( max_length = 100, blank = True)
+	street_line2 = models.CharField( max_length = 100, blank = True)
