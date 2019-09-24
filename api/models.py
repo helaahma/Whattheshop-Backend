26b275bd930fe243dd3dd6a40c49499e9c98f3f6
Saoud Alfares <sfares85@gmail.com>
2019-09-24 22:15:09 +0300

    editing

--- api/models.py
+++ api/models.py
@@ -28,7 +28,7 @@ class Watch(models.Model):
                                        MaxValueValidator(70)],blank=True,null=True)
     water_resistance = models.IntegerField(validators=[MinValueValidator(0),
                                        MaxValueValidator(20000)])
-    manufacture_year = models.IntegerField(validators=[MinValueValidator(1700)])
+    manufacture_year = models.IntegerField(validators=[MinValueValidator(1700)],blank=True,null=True)
     price = models.DecimalField(max_digits=10, decimal_places=3)
     user = models.ForeignKey(User, on_delete=models.CASCADE,  related_name="watches")
     created = models.DateTimeField(auto_now_add=True)
@@ -36,7 +36,7 @@ class Watch(models.Model):
     availability = models.BooleanField(default=True)
     box_papers = models.CharField(max_length=100)
     strap = models.CharField(max_length=100,blank=True,null=True)
-    functions = models.CharField(max_length=100)
+    functions = models.CharField(max_length=100,blank=True,null=True)
 
     MOVEMENT_CHOICES = [
         ('automatic', 'automatic'),
